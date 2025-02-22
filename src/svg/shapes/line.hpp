a424fdb64d60df42739abdc37242f88c79a4eef0
cosmic-striker <vishald.cs2023@citchennai.net>
2025-02-22 19:18:19 +0530

    [Feat] SVG shapes color and vector can change

--- src/svg/shapes/line.hpp
+++ src/svg/shapes/line.hpp
@@ -1,16 +1,23 @@
 #pragma once
-#include "shape.hpp"
-#include <string>
+#include"shape.hpp"
+#include<string>
+#include<sstream>
 
 class Line : public Shape {
 public:
-    Line(double x1, double y1, double x2, double y2);
-
-    std::string toSVG() const override;
+    Line(double x1, double y1, double x2, double y2, std::string& stroke)
+    : x1_(x1), y1_(y1), x2_(x2), y2_(y2), stroke_(stroke) {}
+    std::string toSVG() const override {
+        // Example: <line x1="0" y1="0" x2="100" y2="100" stroke="black" />
+        std::stringstream ss;
+        ss << "<line x1=\"" << x1_ << "\" y1=\"" << y1_ << "\" x2=\"" << x2_ << "\" y2=\"" << y2_ << "\" stroke=\"" << stroke_ << "\"/>";
+        return ss.str();
+    }
 
 private:
     double x1_;
     double y1_;
     double x2_;
     double y2_;
+    std::string stroke_;
 };
--- src/svg/shapes/path.hpp
+++ src/svg/shapes/path.hpp
@@ -1,14 +1,23 @@
 #pragma once
-#include "shape.hpp"
-#include <string>
+#include"shape.hpp"
+#include<string>
+#include<sstream>
 
 // A simple Path class that holds an SVG path string (e.g., "M10 10 H 80 V 80 H 10 Z").
 class Path : public Shape {
 public:
-    explicit Path(const std::string& d);  // 'd' is the "d" attribute in <path d="..."/>
+    explicit Path(const std::string& d, std::string& stroke, std::string& fill)
+        : d_(d), stroke_(stroke), fill_(fill) {} // 'd' is the "d" attribute in <path d="..."/>
     
-    std::string toSVG() const override;
+    std::string toSVG() const override  {
+        // Example: <path d="M10 10 H 80 V 80 H 10 Z" stroke="black" fill="transparent"/>
+        std::stringstream ss;
+        ss << "<path d=\"" << d_ << "\" stroke=\"" << stroke_ << "\" fill=\"" << fill_ << "\"/>";
+        return ss.str();
+    }
 
 private:
     std::string d_;
+    std::string stroke_;
+    std::string fill_;
 };
